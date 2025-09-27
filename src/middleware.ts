b3c8f60006b293799e12cc97857acafb52f6b165
rickyseptiaji <rickyseptiaji2k00@gmail.com>
2025-09-27 23:37:18 +0700

    middleware

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("token")?.value || null;
+  const { pathname } = request.nextUrl;
+  if (!token && pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  if (token && (pathname === "/login" || pathname === "/register")) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+  return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/dashboard/:path*", "/login", "/register"],
+};
