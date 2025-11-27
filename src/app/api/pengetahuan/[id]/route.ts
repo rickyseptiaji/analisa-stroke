a44a06b201d5a6e48b38f1b826b33e453b705455
rickyseptiaji <rickyseptiaji2k00@gmail.com>
2025-11-28 00:11:32 +0700

    up

--- src/app/api/pengetahuan/[id]/route.ts
+++ src/app/api/pengetahuan/[id]/route.ts
@@ -1,112 +1,85 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import prisma from "../../../../../lib/prisma";
 
 export async function GET(
-  req: NextRequest,
+  req: Request,
   { params }: { params: { id: string } }
 ) {
   const id = Number(params.id);
+
   try {
     const pengetahuan = await prisma.pengetahuan.findUnique({
-      where: {
-        id: id,
-      },
+      where: { id },
     });
 
     if (!pengetahuan) {
       return NextResponse.json(
-        {
-          message: "Pengetahuan tidak ditemukan",
-        },
-        {
-          status: 404,
-        }
+        { message: "Pengetahuan tidak ditemukan" },
+        { status: 404 }
       );
     }
+
     return NextResponse.json(pengetahuan, { status: 200 });
   } catch (error) {
     return NextResponse.json(
-      {
-        message: "Internal server error",
-      },
+      { message: "Internal server error" },
       { status: 500 }
     );
   }
 }
 
 export async function PATCH(
-  req: NextRequest,
+  req: Request,
   { params }: { params: { id: string } }
 ) {
   const id = Number(params.id);
   const body = await req.json();
+
   try {
     const updated = await prisma.pengetahuan.update({
-      where: {
-        id: id,
-      },
+      where: { id },
       data: body,
     });
 
-    return NextResponse.json(updated, {
-      status: 200,
-    });
+    return NextResponse.json(updated, { status: 200 });
   } catch (error) {
     return NextResponse.json(
-      {
-        message: "Internal server error",
-      },
-      {
-        status: 500,
-      }
+      { message: "Internal server error" },
+      { status: 500 }
     );
   }
 }
 
 export async function DELETE(
-  req: NextRequest,
+  req: Request,
   { params }: { params: { id: string } }
 ) {
   const id = Number(params.id);
+
   try {
     const pengetahuan = await prisma.pengetahuan.findUnique({
-      where: {
-        id: id,
-      },
+      where: { id },
     });
 
     if (!pengetahuan) {
       return NextResponse.json(
-        {
-          message: "Pengetahuan tidak ditemukan",
-        },
-        {
-          status: 404,
-        }
+        { message: "Pengetahuan tidak ditemukan" },
+        { status: 404 }
       );
     }
 
     await prisma.pengetahuan.delete({
-      where: {
-        id: id,
-      },
+      where: { id },
     });
+
     return NextResponse.json(
-      {
-        message: "Data berhasil dihapus",
-      },
-      {
-        status: 200,
-      }
+      { message: "Data berhasil dihapus" },
+      { status: 200 }
     );
   } catch (error) {
     return NextResponse.json(
-      {
-        message: "Internal server error",
-      },
-      {
-        status: 500,
-      }
+      { message: "Internal server error" },
+      { status: 500 }
     );
   }
 }
