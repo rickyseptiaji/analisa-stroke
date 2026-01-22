c82ffda57681a6ca86b650d969366e59a4787950
rickyseptiaji <rickyseptiaji2k00@gmail.com>
2026-01-22 14:36:22 +0700

    update logic

--- src/app/api/konsultasi/[id]/route.ts
+++ src/app/api/konsultasi/[id]/route.ts
@@ -5,7 +5,7 @@ import { simpanDiagnosa } from "@/lib/simpanDiagnosa";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: Promise<{ id: string }> }
+  { params }: { params: Promise<{ id: string }> },
 ) {
   try {
     const { id } = await params;
@@ -23,7 +23,7 @@ export async function GET(
     if (!hasil) {
       return NextResponse.json(
         { message: "Data tidak ditemukan" },
-        { status: 404 }
+        { status: 404 },
       );
     }
 
@@ -31,47 +31,110 @@ export async function GET(
   } catch (error) {
     return NextResponse.json(
       { message: "Internal Server Error" },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
 
 export async function PATCH(
   req: NextRequest,
-  { params }: { params: Promise<{ id: string }> }
+  { params }: { params: Promise<{ id: string }> },
 ) {
   const { id } = await params;
   const hasilId = Number(id);
-  const { pasienId, jawaban } = await req.json();
+  // const { pasienId, jawaban } = await req.json();
 
-  const pengetahuan = await prisma.pengetahuan.findMany();
-  const hasilHitung = hitungDiagnosa(jawaban, pengetahuan);
+  // const pengetahuan = await prisma.pengetahuan.findMany();
+  // const hasilHitung = hitungDiagnosa(jawaban, pengetahuan);
 
-  if (!hasilHitung.kd_penyakit) {
-    return NextResponse.json(
-      { message: "Tidak ada diagnosa yang cocok" },
-      { status: 400 }
-    );
-  }
+  // if (!hasilHitung.kd_penyakit) {
+  //   return NextResponse.json(
+  //     { message: "Tidak ada diagnosa yang cocok" },
+  //     { status: 400 }
+  //   );
+  // }
 
-  await simpanDiagnosa({
-    hasilId,
-    pasienId: Number(pasienId),
-    kd_penyakit: hasilHitung.kd_penyakit,
-    jawaban: hasilHitung.jawaban,
+  // await simpanDiagnosa({
+  //   hasilId,
+  //   pasienId: Number(pasienId),
+  //   kd_penyakit: hasilHitung.kd_penyakit,
+  //   jawaban: hasilHitung.jawaban,
+  // });
+  const body = await req.json();
+  const pasienId: number = body.pasienId;
+  const jawaban: string[] = body.jawaban;
+
+  const gejala = await prisma.gejala.findMany({
+    where: {
+      kd_gejala: { in: jawaban },
+    },
+
+    select: {
+      kd_gejala: true,
+      poin_gejala: true,
+    },
   });
 
+  const bobot = {
+    ringan: 1,
+    sedang: 2,
+    berat: 3,
+  };
+
+  let tingkatTertinggi: "ringan" | "sedang" | "berat" = "ringan";
+  let nilaiTertinggi = 0;
+
+  for (const g of gejala) {
+    const nilai = bobot[g.poin_gejala as keyof typeof bobot];
+    if (nilai > nilaiTertinggi) {
+      nilaiTertinggi = nilai;
+      tingkatTertinggi = g.poin_gejala as typeof tingkatTertinggi;
+    }
+  }
+
+  const penyakit = await prisma.penyakit.findFirst({
+    where: {
+      nama_penyakit: tingkatTertinggi,
+    },
+    select: {
+      kd_penyakit: true,
+      nama_penyakit: true,
+    },
+  });
+
+  if (!penyakit) {
+    return NextResponse.json(
+      { message: "Tidak ada diagnosa yang cocok" },
+      { status: 400 },
+    );
+  }
+  await prisma.hasil.update({
+    where: { id: hasilId },
+    data: {
+      pasienId: Number(pasienId),
+      kd_penyakit: penyakit.kd_penyakit,
+    },
+  });
+  await prisma.diagnosaDetail.deleteMany({
+    where: { hasilId },
+  });
+  await prisma.diagnosaDetail.createMany({
+    data: jawaban.map((kd_gejala: string) => ({
+      hasilId,
+      kd_gejala,
+    })),
+  });
   return NextResponse.json(
     { message: "Diagnosa berhasil diperbarui" },
-    { status: 200 }
+    { status: 200 },
   );
 }
 
 export async function DELETE(
   req: NextRequest,
-  { params }: { params: Promise<{ id: string }> }
+  { params }: { params: Promise<{ id: string }> },
 ) {
-  const {id} = await params;
+  const { id } = await params;
   const numericId = Number(id);
 
   if (isNaN(numericId)) {
@@ -102,20 +165,20 @@ export async function DELETE(
 
     return NextResponse.json(
       { message: "Data berhasil dihapus" },
-      { status: 200 }
+      { status: 200 },
     );
   } catch (error: any) {
     if (error.message === "NOT_FOUND") {
       return NextResponse.json(
         { message: "Data tidak ditemukan" },
-        { status: 404 }
+        { status: 404 },
       );
     }
 
     console.error(error);
     return NextResponse.json(
       { message: "Internal Server Error" },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
--- src/app/api/konsultasi/route.ts
+++ src/app/api/konsultasi/route.ts
@@ -4,25 +4,85 @@ import { hitungDiagnosa } from "@/lib/hitungDiagnosa";
 import { simpanDiagnosa } from "@/lib/simpanDiagnosa";
 
 export async function POST(req: NextRequest) {
-  const { pasienId, jawaban } = await req.json();
+  const body = await req.json();
+  const pasienId: number = body.pasienId;
+  const jawaban: string[] = body.jawaban;
 
-  const pengetahuan = await prisma.pengetahuan.findMany();
-  const hasilHitung = hitungDiagnosa(jawaban, pengetahuan);
+  const gejala = await prisma.gejala.findMany({
+    where: {
+      kd_gejala: { in: jawaban },
+    },
 
-  if (!hasilHitung.kd_penyakit) {
-    return NextResponse.json(
-      { message: "Tidak ada diagnosa yang cocok" },
-      { status: 400 }
-    );
-  }
-
-  await simpanDiagnosa({
-    pasienId: Number(pasienId),
-    kd_penyakit: hasilHitung.kd_penyakit,
-    jawaban: hasilHitung.jawaban,
+    select: {
+      kd_gejala: true,
+      poin_gejala: true,
+    },
   });
 
+  const bobot = {
+    ringan: 1,
+    sedang: 2,
+    berat: 3,
+  };
+
+  let tingkatTertinggi: "ringan" | "sedang" | "berat" = "ringan";
+  let nilaiTertinggi = 0;
+
+  for (const g of gejala) {
+    const nilai = bobot[g.poin_gejala as keyof typeof bobot];
+    if (nilai > nilaiTertinggi) {
+      nilaiTertinggi = nilai;
+      tingkatTertinggi = g.poin_gejala as typeof tingkatTertinggi;
+    }
+  }
+
+  const penyakit = await prisma.penyakit.findFirst({
+    where: {
+      nama_penyakit: tingkatTertinggi,
+    },
+    select: {
+      kd_penyakit: true,
+      nama_penyakit: true,
+    },
+  });
+
+  if (!penyakit) {
+    return NextResponse.json(
+      { message: "Tidak ada diagnosa yang cocok" },
+      { status: 400 },
+    );
+  }
+  const hasil = await prisma.hasil.create({
+    data: {
+      pasienId: Number(pasienId),
+      kd_penyakit: penyakit.kd_penyakit,
+    },
+  });
+
+  await prisma.diagnosaDetail.createMany({
+    data: jawaban.map((kd_gejala) => ({
+      hasilId: hasil.id,
+      kd_gejala,
+    })),
+  });
   return NextResponse.json({ message: "Diagnosa berhasil" }, { status: 200 });
+  // const pengetahuan = await prisma.pengetahuan.findMany();
+  // const hasilHitung = hitungDiagnosa(jawaban, pengetahuan);
+
+  // if (!hasilHitung.kd_penyakit) {
+  //   return NextResponse.json(
+  //     { message: "Tidak ada diagnosa yang cocok" },
+  //     { status: 400 }
+  //   );
+  // }
+
+  // await simpanDiagnosa({
+  //   pasienId: Number(pasienId),
+  //   kd_penyakit: hasilHitung.kd_penyakit,
+  //   jawaban: hasilHitung.jawaban,
+  // });
+
+  // return NextResponse.json({ message: "Diagnosa berhasil" }, { status: 200 });
 }
 
 export async function GET() {
@@ -37,5 +97,3 @@ export async function GET() {
     return NextResponse.json(error);
   }
 }
-
-
